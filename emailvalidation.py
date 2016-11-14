2b6220265f6ffde05521d7df5f03f69aa85055a7
Diana Pechter <dianapechter@Dianas-MacBook-Air.local>
2016-11-13 20:03:26 -0500

    Check for length

--- emailvalidation.py
+++ emailvalidation.py
@@ -36,6 +36,10 @@ def testPeriods(email):
 	firstPart = email.split("@")[0] 
 	lastPart = email.split("@")[1]
 
+	# Check for a period
+	if email.count(".") is 0:
+		return
+
 	# Check whether the last part has more than 1 period
 	numberOfPeriods = lastPart.count(".")
 	if numberOfPeriods > 1 :
@@ -55,6 +59,19 @@ def testPeriods(email):
 	if positionOfPeriod is positionOfLastCharacterOfFirstPart:
 		raise SystemExit("Sorry! This is not a valid email. The first part of your email cannot end with a period.")	
 
+# Check length
+def testLength(email):
+	# Partition email into two strings separated by @
+	firstPart = email.split("@")[0] 
+	lastPart = email.split("@")[1]
+
+	# Check that the first part is less than 64 characters
+	if len(firstPart) > 63:
+		raise SystemExit("Sorry! This is not a valid email. The first part of your email is too long.")	
+
+	# Check that the last part is less than 255 characters
+	if len(lastPart) > 254:
+		raise SystemExit("Sorry! This is not a valid email. The last part of your email is too long.")	
 
 # Input your email
 print("Your Very Own Email Validator!")
@@ -62,7 +79,7 @@ email = raw_input("Type your email: ")
 
 testATs(email)
 testPeriods(email)
-
+testLength(email)
 
 print(email+" is a valid email. We can spam you now! Woo!")
 
